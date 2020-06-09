7053c4a41d554efde11626142afff8379376d8b0
Ubuntu <loic.minier@ubuntu.com>
2020-06-09 17:57:10 +0000

    UBUNTU: [Packaging] Tighten up Xilinx flavour

--- debian.xilinx/rules.d/arm64.mk
+++ debian.xilinx/rules.d/arm64.mk
@@ -1,24 +1,30 @@
+skipabi         = true
+
 human_arch	= ARMv8
 build_arch	= arm64
 header_arch	= arm64
 defconfig	= defconfig
-flavours	= generic
-build_image_generic	= Image.gz
-kernel_file_generic	= arch/$(build_arch)/boot/Image.gz
-install_file	= vmlinuz
-no_dumpfile = true
-uefi_signed     = true
+flavours	= xilinx
+build_image	= Image
+kernel_file	= arch/$(build_arch)/boot/Image
+install_file	= vmlinux
+no_dumpfile     = true
 
 loader		= grub
-vdso		= vdso_install
+#vdso		= vdso_install
 
 do_extras_package = true
 do_tools_usbip  = true
 do_tools_cpupower = true
 do_tools_perf   = true
-do_tools_perf_jvmti = true
 do_tools_bpftool = true
+
+do_common_headers_indep=false
 
 do_dtbs		= true
 do_zfs		= true
+disable_d_i = true
+do_libc_dev_package=false
+do_doc_package  = false
+do_source_package= false
 do_dkms_wireguard = true
